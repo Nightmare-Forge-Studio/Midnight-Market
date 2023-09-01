eb52739ac393aef393a166a635e5b577fd83473f
Siv3rn <anggito732@gmail.com>
2023-09-01 15:04:37 +0700

    Player Controller

---
+++ Assets/Prototype1Script/FlashlightController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+    public Transform camera;
+    public Light light;
+    private bool drainOverTime = true;
+
+    public float minBrightness;
+
+    public float maxBrightness;
+
+    public float drainRate;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.rotation = camera.rotation;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            light.enabled = !light.enabled;
+        }
+
+        Mathf.Clamp(light.intensity, minBrightness, maxBrightness);
+        if (drainOverTime && light.enabled)
+        {
+
+            if (light.intensity > minBrightness)
+            {
+                light.intensity -= Time.deltaTime * (drainRate/1000);
+            }
+        }
+
+    }
+    
+    
+}
