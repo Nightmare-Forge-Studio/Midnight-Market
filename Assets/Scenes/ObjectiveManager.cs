b6988141d15d64274dd7c91bd1dd85791e042579
satrio adi <143088360+kale16@users.noreply.github.com>
2023-09-20 10:46:24 +0700

    tambahin features objective

---
+++ Assets/Scenes/ObjectiveManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    public ObjectiveController[] objectiveDatas;
+
+   public void AddObjective(ObjectiveData objective)
+   {
+    
+   }
+
+   public void RemoveObjective(ObjectiveData objective)
+   {
+    
+   }
+}
